6ada384a16b5deb630303ee9b6032d9184f04cc3
Fadi Albahrani <business.fadi21@gmail.com>
2021-07-13 12:45:11 +0300

    task1 and task2 .js added

---
+++ task1.js
@@ -0,0 +1,17 @@
+
+
+
+function student(id, sName, sLevel) {
+    return student = {
+        studentId: id,
+        studentName: sName,
+        studentLevel: sLevel
+
+    }
+
+    
+
+}
+
+student(20201524, Fadi, Senior);
+console.log(student.studentId);
